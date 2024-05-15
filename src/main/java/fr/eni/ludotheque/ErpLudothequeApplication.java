b444d2d7bfff850d148d7e63a399c59744b92110
Mathieu CHARRON <mathieu.charron2022@campus-eni.fr>
2024-05-15 14:30:36 +0200

    first commit

---
+++ src/main/java/fr/eni/ludotheque/ErpLudothequeApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.ludotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpLudothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpLudothequeApplication.class, args);
+	}
+
+}
